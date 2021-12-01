98f5450e0f5bda49cc4782bc89e7bfdb69647457
kyle01 <kylemcv@gmail.com>
2021-12-01 11:03:04 -0500

    Complete day 1 part 2

--- 2021/01/ruby.rb
+++ 2021/01/ruby.rb
@@ -1,10 +1,24 @@
 file = File.open('input.txt')
 numbers = file.readlines.map(&:chomp)
 
+# part 1
 increment_count = 0 
 k = 1
 while k < numbers.length 
     if numbers[k].to_i > numbers[k-1].to_i
+        increment_count += 1
+    end
+    k += 1
+end
+puts increment_count
+
+# Part 2
+increment_count = 0 
+k = 3
+while k < numbers.length
+    window_one = numbers[k-3].to_i + numbers[k-2].to_i + numbers[k-1].to_i
+    window_two = numbers[k-2].to_i + numbers[k-1].to_i + numbers[k].to_i
+    if window_two > window_one
         increment_count += 1
     end
     k += 1
