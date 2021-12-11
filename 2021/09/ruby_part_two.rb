3bf5e6589051d24b300e1c246ac1f5dc14707b0d
kyle01 <kylemcv@gmail.com>
2021-12-11 12:26:27 -0500

    Complete day 9 part 2

--- 2021/09/ruby_part_two.rb
+++ 2021/09/ruby_part_two.rb
@@ -1,65 +1,58 @@
 file = File.open('input.txt')
 lines = file.readlines.map(&:chomp)
 
-def basin_size(survey, row_index, column_index, current_size, known_spots)
-    puts "row_index: #{row_index}, column_index: #{column_index}, current_size: #{current_size}"
-    # check up 
-    if row_index - 1 >= 0 && known_spots.none?{|x| x == "#{row_index - 1},#{column_index}"}
-        next_spot = survey[row_index - 1][column_index]
-        if next_spot != 9
-            basin_size(survey, row_index - 1, column_index, current_size + 1, known_spots << "#{row_index},#{column_index}")
+def find_basin_size(survey, col, row)
+    seen = []
+    size = 0
+    queue = [[col, row]]
+    while queue.length > 0
+        current = queue.shift
+        size += 1
+        seen << current
+        if current[0] + 1 < survey[0].length
+            next_point = [current[0] + 1, current[1]]
+            queue << next_point if !seen.include?(next_point) && survey[next_point[1]][next_point[0]] != 9 && !queue.include?(next_point)
+        end
+        if current[0] - 1 >= 0
+            next_point = [current[0] - 1, current[1]]
+            queue << next_point if !seen.include?(next_point) && survey[next_point[1]][next_point[0]] != 9 && !queue.include?(next_point)
+        end
+        if current[1] + 1 < survey.length
+            next_point = [current[0], current[1] + 1]
+            queue << next_point if !seen.include?(next_point) && survey[next_point[1]][next_point[0]] != 9 && !queue.include?(next_point)
+        end
+        if current[1] - 1 >= 0
+            next_point = [current[0], current[1] - 1]
+            queue << next_point if !seen.include?(next_point) && survey[next_point[1]][next_point[0]] != 9 && !queue.include?(next_point)
         end
     end
-    # check down
-    if row_index + 1 < survey.length - 1 && known_spots.none?{|x| x == "#{row_index + 1},#{column_index}"}
-        next_spot = survey[row_index + 1][column_index]
-        if next_spot != 9 
-            basin_size(survey, row_index + 1, column_index, current_size + 1, known_spots << "#{row_index},#{column_index}")
-        end
-    end
-    # check left
-    if column_index - 1 >= 0 && known_spots.none?{|x| x == "#{row_index},#{column_index - 1}"}
-        next_spot = survey[row_index][column_index - 1]
-        if next_spot != 9
-            basin_size(survey, row_index, column_index - 1, current_size + 1, known_spots << "#{row_index},#{column_index}")
-        end
-    end
-    # check right
-    if column_index + 1 < survey[row_index].length - 1 && known_spots.none?{|x| x == "#{row_index},#{column_index + 1}"}
-        next_spot = survey[row_index][column_index + 1]
-        if next_spot != 9 
-            basin_size(survey, row_index, column_index + 1, current_size + 1, known_spots << "#{row_index},#{column_index}")
-        end
-    end
-    current_size
+    size
 end
 
-def find_low_points(survey)
-    basin_sizes = []
+def find_basin_sizes(survey)
+    low_points = []
     survey.each_with_index do |row, row_index|
-        row.each_with_index do |point, column_index|
+        row.each_with_index do |point, point_index|
             checker = true
             if row_index + 1 < survey.length 
-                checker = false if point >= survey[row_index + 1][column_index]
+                checker = false if point >= survey[row_index + 1][point_index]
             end
             if row_index - 1 >= 0
-                checker = false if point >= survey[row_index - 1][column_index]
+                checker = false if point >= survey[row_index - 1][point_index]
             end
-            if column_index + 1 < row.length
-                checker = false if point >= survey[row_index][column_index + 1]
+            if point_index + 1 < row.length
+                checker = false if point >= survey[row_index][point_index + 1]
             end
-            if column_index - 1 >= 0
-                checker = false if point >= survey[row_index][column_index - 1]
+            if point_index - 1 >= 0
+                checker = false if point >= survey[row_index][point_index - 1]
             end
-            if checker && survey[row_index][column_index] != 1 && basin_sizes.length < 1
-                basin_sizes << basin_size(survey, row_index, column_index, 1, [])
-            end
+            low_points << find_basin_size(survey, point_index, row_index) if checker
         end
     end
-    puts basin_sizes.join(',')
-    basin_sizes.max(3)
+    low_points
 end
 
 survey = []
 lines.each{|line| survey << line.split('').map(&:to_i)}
-find_low_points(survey).reduce(:*)
+basin_sizes = find_basin_sizes(survey)
+puts basin_sizes.max(3).reduce(:*)
