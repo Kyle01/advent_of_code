f347683ff1d1aae1a9ca668d4a0873b245d323b4
kyle01 <kylemcv@gmail.com>
2021-12-24 15:34:58 -0500

    Attempt part 1 day 24

--- 2021/24/ruby.rb
+++ 2021/24/ruby.rb
@@ -1,24 +1,11 @@
 file = File.open('input.txt')
 lines = file.readlines.map(&:chomp)
 
-def create_algorithsm(lines)
-    algorithms = []
-
-    algorithm = nil
-    lines.each do |line|
-        if line.split(' ')[0] == 'inp'
-            algorithms << algorithm if algorithm
-            algorithm = [line]
-        else
-            algorithm << line
-        end
-    end
-    algorithms << algorithm
-end
-
-def is_valid?(algoirthms, number)
+def is_valid?(lines, number)
     w, x, y, z = 0, 0, 0, 0
-    algoirthms.each do |line|
+    
+    k = 0
+    lines.each do |line|
         operation = line.split(' ')[0]
         a = line.split(' ')[1]
         b = line.split(' ')[2]
@@ -36,7 +23,8 @@ def is_valid?(algoirthms, number)
         end
 
         if operation == 'inp'
-            w = digit
+            w = number.to_s[k].to_i
+            k += 1
         elsif operation == 'add'
             if a == 'x'
                 x += b
@@ -55,13 +43,25 @@ def is_valid?(algoirthms, number)
             end
         elsif operation == 'mod'
             if a == 'x'
+                if x < 0 || b <= 0 
+                    skip
+                end
                 x %= b
             elsif a == 'y'
+                if y < 0 || b <= 0 
+                    skip
+                end
                 y %= b
             elsif a == 'z'
+                if z < 0 || b <= 0 
+                    skip
+                end
                 z %= b 
             end
         elsif operation == 'div'
+            if b == 0
+                next
+            end
             if a == 'x'
                 x /= b
             elsif a == 'y'
@@ -73,9 +73,9 @@ def is_valid?(algoirthms, number)
             if a == 'x'
                 x = x == b ? 1 : 0
             elsif a == 'y'
-                x = x == b ? 1 : 0
+                y = x == b ? 1 : 0
             elsif a == 'z'
-                x = x == b ? 1 : 0
+                z = x == b ? 1 : 0
             end
         end
     end
@@ -84,5 +84,12 @@ def is_valid?(algoirthms, number)
     z == 0
 end
 
-algorithms = create_algorithsm(lines)
-puts is_valid?(algorithms, 13579246899999)
+k = 99999999999999
+# while k >= 11111111111111
+#     if is_valid?(lines, k)
+#         puts k
+#         break
+#     end
+#     k -= 1
+# end
+puts is_valid?(lines, 1)
