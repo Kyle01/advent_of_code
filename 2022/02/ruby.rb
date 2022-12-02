87ca44c83ae109edf1b281d7814efa65f830096c
Kyle McVeigh <kylemcv@gmail.com>
2022-12-02 09:35:46 -0500

    Complete day 2 part 2'

--- 2022/02/ruby.rb
+++ 2022/02/ruby.rb
@@ -1,37 +1,48 @@
 lines = File.open('input.txt').readlines.map(&:chomp)
 
-# win is 6, tie is 3, loss is 0 
-# rock is 1, paper is 2, scissors is 3
-# a, x is rock 
-# b, y is paper 
-# c, z is scissors
-
-
-a = "ROCK" 
-b = "PAPER"
-c = "SCISSORS"
-
 def score_challenge(a, b)
-    return 3 if a == 'A' && b == 'X' 
-    return 3 if a == 'B' && b == 'Y' 
-    return 3 if a == 'C' && b == 'Z' 
+    return 3 if a == 'A' && b == 'A' 
+    return 3 if a == 'B' && b == 'B' 
+    return 3 if a == 'C' && b == 'C' 
 
-    return 6 if a == 'A' && b == 'Y' 
-    return 6 if a == 'B' && b == 'Z' 
-    return 6 if a == 'C' && b == 'X' 
+    return 6 if a == 'A' && b == 'B' 
+    return 6 if a == 'B' && b == 'C' 
+    return 6 if a == 'C' && b == 'A' 
 
     0
 end
 
 def score_throw move
-    return 1 if move === 'X'
-    return 2 if move === 'Y'
+    return 1 if move === 'A'
+    return 2 if move === 'B'
     
     3
 end
 
 def score_round line 
-    a, b = line.split(' ')[0], line.split(' ')[1]
+    a = line.split(' ')[0]
+    b = nil
+
+    case line.split(' ')[1]
+    when 'X'
+        if a == 'A'
+            b = 'C'
+        elsif a == 'B'
+            b = 'A'
+        else
+            b = 'B'
+        end
+    when 'Y'
+        b = a 
+    else
+        if a == 'A'
+            b = 'B'
+        elsif a == 'B'
+            b = 'C'
+        else
+            b = ''
+        end
+    end
 
     score_throw(b) + score_challenge(a,b)
 end
