2a511c6a82199687a9840d624565beb699eeef3a
Kyle McVeigh <kylemcv@gmail.com>
2022-12-03 08:31:05 -0500

    Day day 3 part 1

---
+++ 2022/03/ruby.rb
@@ -0,0 +1,30 @@
+def compute_letters line 
+    first,second = line.partition(/.{#{line.size/2}}/)[1,2]    
+    answer_char = nil
+
+    first.split('').each do |char|
+        if second.include?(char) 
+            answer_char = char
+        end
+    end
+
+    answer_char
+end
+
+def letter_values letters 
+    lower_arr = ("a".."z").to_a
+    upper_arr = ("A".."Z").to_a
+    key = ['.'].concat(lower_arr).concat(upper_arr)
+    values = []
+
+    letters.each {|letter| values << key.find_index(letter) }
+    
+    values
+end
+
+lines = File.open('input.txt').readlines.map(&:chomp)
+
+key_letters = []
+lines.each { |line| key_letters << compute_letters(line) }
+values = letter_values(key_letters)
+puts values.inject(:+)
