57c0a2f8419be9c1aee7a984ca345709d54f71d5
Kyle McVeigh <kylemcv@gmail.com>
2022-12-04 21:31:02 -0500

    Complete day 4 part 1

---
+++ 2022/04/ruby.rb
@@ -0,0 +1,16 @@
+def fully_contained?(line)
+    a, b = line.split(',')
+    arr_one = (a.split('-')[0]..a.split('-')[1]).to_a
+    arr_two = (b.split('-')[0]..b.split('-')[1]).to_a
+
+    return true if arr_one.all? { |e| arr_two.include?(e) }
+    return true if arr_two.all? { |e| arr_one.include?(e) }
+
+    false
+end
+
+count = 0 
+lines = File.open('input.txt').readlines.map(&:chomp)
+lines.each { |line| count += 1 if fully_contained?(line) }
+
+puts count
