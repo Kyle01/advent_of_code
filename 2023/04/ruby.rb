3c40c5fdf28113a779882474ab64d02095d3f699
kyle01 <kylemcv@gmail.com>
2023-12-04 00:21:05 -0500

    Complete day 4 part 2

--- 2023/04/ruby.rb
+++ 2023/04/ruby.rb
@@ -1,22 +1,25 @@
 lines = File.open('input.txt').readlines.map(&:chomp)
 score = 0
+card_count = Array.new(lines.count, 1)
 
-lines.each do |line|
+lines.each_with_index do |line, i|
+    puts i + 1
     winning_numbers = line.split(" | ")[0].split(": ")[1].split(" ").map(&:to_i)
     card_numbers = line.split(" | ")[1].split(" ").map(&:to_i)
-    card_score = 0 
 
-    card_numbers.each do |num|
-        if winning_numbers.include?(num)
-            if card_score == 0 
-                card_score += 1
-            else
-                card_score *= 2
-            end
+    card_count[i].times do 
+        card_score = 0 
+    
+        card_numbers.each do |num|
+            card_score += 1 if winning_numbers.include?(num)
+        end
+
+        k = i + 1
+        card_score.times do
+            card_count[k] += 1
+            k += 1
         end
     end
-    
-    score += card_score
 end
 
-puts score
+puts card_count.reduce(:+)
