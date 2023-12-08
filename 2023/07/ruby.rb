e8b30c5e4f0dd3845b0563c4cc901cc454ceb77c
kyle01 <kylemcv@gmail.com>
2023-12-07 22:07:38 -0500

    Complete day 7 part 2

--- 2023/07/ruby.rb
+++ 2023/07/ruby.rb
@@ -8,10 +8,49 @@ class Hand
     end
 
     def to_s
-        "#{@hand} - #{@bid} - #{card_type}"
+        "#{@hand} - #{@bid} - #{@hand.include?('J')} - #{card_type}"
     end
 
     def card_type
+        return classic_card_type unless @hand.include?('J')
+        classic_type = classic_card_type
+        j_count = @hand.split('').count{|x| x == 'J'}
+
+        return 'fives' if j_count == 5
+        return 'fives' if j_count == 4
+
+        if j_count == 3 
+            return 'fives' if classic_type == 'full_house'
+            return 'fours'
+        elsif j_count == 2 
+            return 'fives' if classic_type == 'full_house'
+            return 'fours' if classic_type == 'two_pair'
+            return 'threes'
+        end
+
+        return 'fives' if classic_type == 'fours'
+        return 'fours' if classic_type == 'threes'
+        return 'full_house' if classic_type == 'two_pair'
+        return 'threes' if classic_type == 'one_pair'
+        
+        'one_pair'
+    end
+
+    def <=>(b)
+        card_rank = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'].reverse
+        type_rank = ['fives', 'fours', 'full_house', 'threes', 'two_pair', 'one_pair', 'high_card']
+
+        return type_rank.find_index(b.card_type) - type_rank.find_index(card_type) if b.card_type != card_type
+
+        @hand.split("").each_with_index do |card, i|
+            b_card = b.hand[i]
+            return card_rank.find_index(card) - card_rank.find_index(b_card) if card != b_card
+        end
+    end
+
+    private 
+    
+    def classic_card_type 
         cards = @hand.split('')
         count_arr = []
         cards.each do |card|
@@ -27,31 +66,8 @@ class Hand
         'high_card'
     end
 
-    def <=>(b)
-        card_rank = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'].reverse
-        type_rank = ['fives', 'fours', 'full_house', 'threes', 'two_pair', 'one_pair', 'high_card']
-
-        return type_rank.find_index(b.card_type) - type_rank.find_index(card_type) if b.card_type != card_type
-
-        @hand.split("").each_with_index do |card, i|
-            b_card = b.hand[i]
-            return card_rank.find_index(card) - card_rank.find_index(b_card) if card != b_card
-        end
-    end
-
 end
 
-# def sort_cards a, b
-#     card_rank = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-#     type_rank = ['fives', 'fours', 'full_house', 'threes', 'two_pair', 'one_pair', 'high_card']
-
-#     return type_rank.find_index(b.card_type) - type_rank.find_index(a.card_type) if b.card_type != a.card_type
-
-#     a.hand.split("").each_with_index do |card, i|
-#         b_card = b.hand[i]
-#         return card_rank.find_index(card) - card_rank.find_index(b_card) if card != b_card
-#     end
-# end
 
 lines = lines.map{|line| Hand.new(line)}
 lines.sort!
