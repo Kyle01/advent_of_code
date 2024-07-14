309baca8c50d3119520239aa03f1a3d3da5011ae
kyle01 <kylemcv@gmail.com>
2024-07-14 18:01:16 -0400

    Complete day 4 part 1

---
+++ 2019/04/python.py
@@ -0,0 +1,44 @@
+# range 284639-748759
+
+LOWER_RANGE = 284639
+UPPER_RANGE = 748759
+
+num_possibilities = 0
+k = LOWER_RANGE
+
+def has_adjacent_digits(num):
+    chars = list(str(num))
+    if(chars[0] == chars[1]):
+        return True
+    if(chars[1] == chars[2]):
+        return True
+    if(chars[2] == chars[3]):
+        return True
+    if(chars[3] == chars[4]):
+        return True
+    if(chars[4] == chars[5]):
+        return True
+    return False
+
+def digits_never_increase(num):
+    helper_arr = list(str(num))
+    nums = [int(x) for x in helper_arr]
+    if(nums[0] > nums[1]):
+        return False
+    if(nums[1] > nums[2]):
+        return False
+    if(nums[2] > nums[3]):
+        return False
+    if(nums[3] > nums[4]):
+        return False
+    if(nums[4] > nums[5]):
+        return False
+        
+    return True
+
+while k < UPPER_RANGE:
+    if(has_adjacent_digits(k) and digits_never_increase(k)):
+        num_possibilities += 1
+    k += 1
+
+print(num_possibilities)
