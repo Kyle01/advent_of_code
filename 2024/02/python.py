7cb97151e1cd9353962ea0181eafab62d4652f5b
kyle01 <kylemcv@gmail.com>
2024-12-03 17:55:31 -0500

    Complete day 2 part 2

--- 2024/02/python.py
+++ 2024/02/python.py
@@ -1,26 +1,46 @@
+import copy
+
 input = []
+
 with open('./input.txt') as f:
     for line in f:
         input.append(line.strip())
 
-
-def is_safe(line):
-    numbers = [int(x) for x in line.split(' ')]
-    
-    if (numbers[0] > numbers[1]):
-        numbers.reverse()
-    
+def is_safe(numbers):
     k = 1 
     while k < len(numbers):
-        if (numbers[k] - numbers[k-1] < 1 or numbers[k] - numbers[k-1] > 3):
+        if numbers[k] - numbers[k-1] < 1 or numbers[k] - numbers[k-1] > 3:
             return False
         k += 1
     
     return True
 
+def test_line(line):
+    numbers = [int(x) for x in line.split(' ')]
+
+    if is_safe(numbers):
+        return True
+    numbers.reverse()
+    if is_safe(numbers):
+        return True
+    
+    k = 0
+    while k < len(numbers):
+        copy_arr = copy.deepcopy(numbers)
+        copy_arr.pop(k)
+        if is_safe(copy_arr):
+            return True
+        copy_arr.reverse()
+        if is_safe(copy_arr):
+            return True
+        k += 1
+        
+    return False 
+
 answers = 0
 for line in input:
-    if is_safe(line):
+    print(line)
+    if test_line(line):
         answers += 1
 
 print(answers)
