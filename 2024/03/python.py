981ec8a8c18b94edf076923e85fbaf6738e43134
kyle01 <kylemcv@gmail.com>
2024-12-03 22:24:18 -0500

    Complete day 3 part 2

--- 2024/03/python.py
+++ 2024/03/python.py
@@ -2,13 +2,21 @@ import re
 
 with open('input.txt', 'r') as file:
     line = file.readline().strip()
-matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", line)
-
+matches = re.findall(r"(?:mul|do|don't)\(\d{0,3}(?:,\d{1,3})?\)", line)
 answer = 0
+enabled = True
 for match in  matches:
-    match = match[:-1]
-    match = match[4:]
-    numbers = [int(x) for x in match.split(',')]
-    answer += numbers[0] * numbers[1]
+    if match == 'do()':
+        enabled = True
+    elif match == "don't()":
+        enabled = False
+    else:
+        if not enabled:
+            continue
+        else:
+            match = match[:-1]
+            match = match[4:]
+            numbers = [int(x) for x in match.split(',')]
+            answer += numbers[0] * numbers[1]
 
 print(answer)
