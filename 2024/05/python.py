d58815d0a205309c303bca344c7ab6f78b2ab8c0
kyle01 <kylemcv@gmail.com>
2024-12-10 08:13:14 -0500

    Complete day 5 part 2

--- 2024/05/python.py
+++ 2024/05/python.py
@@ -12,9 +12,11 @@ solution = 0
 for line in tests:
     numbers = [int(x) for x in line.split(',')]
     line_valid = True
+    was_ever_false = False
 
-    for rule in rules:
-        rule_nums = [int(x) for x in rule.split('|')]
+    k = 0
+    while k < len(rules) - 1:
+        rule_nums = [int(x) for x in rules[k].split('|')]
         first_num = rule_nums[0]
         second_num = rule_nums[1]
 
@@ -23,10 +25,19 @@ for line in tests:
             if second_num == num:
                 second_seen = True
             if second_seen and first_num == num:
+                was_ever_false = True
                 line_valid = False
 
-    # print(f"{line} ; {line_valid}")
-    if line_valid:
+        if not line_valid:
+            a, b = numbers.index(first_num), numbers.index(second_num)
+            numbers[b], numbers[a] = numbers[a], numbers[b]
+            line_valid = True
+            second_seen = False
+            k = 0
+            continue
+        k += 1
+    
+    if was_ever_false:
         middle_index = int((len(numbers) - 1)/2)
         solution += numbers[middle_index]
 
